af10308c21666754eda55b35b2cc6a62408b450e
TelluriumCrystal <admin@andrewbennet1.com>
2019-02-19 17:42:18 -0500

    Created code template in Atmel Studio

---
+++ lora_test/main.c
@@ -0,0 +1,21 @@
+/*
+ * Name    : lora_test.c
+ * Created : 2/19/2019 5:37:00 PM
+ * Author  : Andrew Bennett
+ *
+ * Description : Code for the Lora Test PCB. Intended to test the E22-900M30S Lora transeiver and the NEO-8M GNSS reciever.
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	// TODO: Write code
+
+    while (1) 
+    {
+		
+    }
+}
+
