7287358e54fdc2f3453356ef1473120125aebfd3
TelluriumCrystal <admin@andrewbennet1.com>
2019-02-21 09:22:25 -0500

    Minor edit to description

--- lora_test/main.c
+++ lora_test/main.c
@@ -3,11 +3,10 @@
  * Created : 2/19/2019 5:37:00 PM
  * Author  : Andrew Bennett
  *
- * Description : Code for the Lora Test PCB. Intended to test the E22-900M30S Lora transeiver and the NEO-8M GNSS reciever.
+ * Description : Code for the Lora Test PCB. Intended to test the E22-900M30S Lora transceiver and the NEO-8M GNSS receiver.
  */ 
 
 #include <avr/io.h>
-
 
 int main(void)
 {
